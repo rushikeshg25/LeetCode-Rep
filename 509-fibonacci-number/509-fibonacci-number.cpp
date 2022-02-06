a01239e41cbb56fa613dfff06bad9ede0271f822
Rushikesh Ghotekar <rushikeshvghotekar25@gmail.com>
2022-02-06 13:25:31 +0530

    Time: 0 ms (100.00%), Space: 6 MB (39.43%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   int fib(int N) {
+        if(N < 2) 
+            return N;
+    	int a = 0, b = 1, c = 0;
+        for(int i = 1; i < N; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+};

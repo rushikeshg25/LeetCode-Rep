5fbac0f08ab866d471fb8b3a2abccfc0a314d730
Rushikesh Ghotekar <rushikeshvghotekar25@gmail.com>
2022-06-27 23:34:34 +0530

    Time: 2 ms (48.14%), Space: 6.1 MB (43.86%) - LeetHub

---
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int cnt=0;
+        for(char i:s){
+            if(i==letter) cnt++; 
+        }
+        return (cnt*100)/s.size();
+    }
+};

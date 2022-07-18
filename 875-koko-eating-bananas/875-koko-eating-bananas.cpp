5ffd1777acc11f4e3e00a41fb46a46fe67272697
Rushikesh Ghotekar <rushikeshvghotekar25@gmail.com>
2022-07-18 23:15:53 +0530

    Time: 83 ms (37.61%), Space: 18.8 MB (68.84%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-  bool is_enough(vector<int>& p, int h, int x){
-    for(auto n: p)
-      if( (h -= (n/x + (bool)(n%x))) < 0) return false; 
-    return true;
-  }
-  
-  int minEatingSpeed(vector<int>& piles, int h) { 
-    int l = 1, r = *max_element(piles.begin(), piles.end()), m;
-    
-    while(l <= r)
-      if (is_enough(piles, h, (m = (l + r)>>1))) r = m - 1; 
-      else l = m + 1; 
-    
-    return l; 
-  }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        int r=1000000000;
+        int time=0;
+        //for(auto i:piles) r=max(r,i);
+        while(l<r){
+            int mid=(l+r)/2;
+            time=0;
+            for(auto i:piles){
+                 time += (i + mid - 1) / mid;
+            }
+            if(time>h) l=mid+1;
+            else r=mid;
+
+        }
+        return l;
+    }
 };

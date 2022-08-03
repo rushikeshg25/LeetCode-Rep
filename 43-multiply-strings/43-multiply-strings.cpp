67cea7340f8ab74ee3fb0d63f978ddde9ec777a3
Rushikesh Ghotekar <rushikeshvghotekar25@gmail.com>
2022-08-04 02:10:49 +0530

    Time: 7 ms (81.96%), Space: 6.4 MB (93.01%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,17 +1,20 @@
-   class Solution {
+class Solution {
 public:
-    string multiply(string& A, string& B) {
-        if(A == "0" || B == "0") return "0";
-        string ans(size(A)+size(B), '0');
-        for(int i = size(A)-1; i >= 0; i--) {
-            for(int j = size(B)-1; j >= 0; j--) {
-                int res = (ans[i+j+1]-'0') + (A[i]-'0') * (B[j]-'0');
-                ans[i+j+1] = res%10 + '0';  
-                ans[i+j] += res/10;         
+    string multiply(string a, string b) {
+        if(a=="0" || b=="0") return "0";
+        int n=a.size();
+        int m=b.size();
+        string ans(size(a)+size(b),'0');
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int mul=(a[i]-'0')*(b[j]-'0');
+                int sum=mul+(ans[i+j+1]-'0');
+                ans[i+j+1] = sum%10 + '0';  
+                ans[i+j] += sum/10; 
             }
         }
         if(ans[0] == '0') return ans.substr(1);
         return ans;
     }
-
 };
+

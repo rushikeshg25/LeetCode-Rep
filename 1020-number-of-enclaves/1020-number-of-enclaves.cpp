57556e1f1f53a1f4bb11a0391c8ea667deac7511
Rushikesh Ghotekar <rushikeshvghotekar25@gmail.com>
2022-08-31 23:44:46 +0530

    Time: 135 ms (16.34%), Space: 24.2 MB (12.87%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -3,9 +3,7 @@ public:
     int d[5]={0,-1,0,1,0};
     int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
-        if(m==1 || n==1){
-            return 0;
-        }
+       
         vector<vector<int>>visited(m,vector<int>(n,0));
         for(int i=0;i<m;i++){
             if(grid[i][0]==1 && !visited[i][0]) dfs(grid,visited,i,0,m,n);

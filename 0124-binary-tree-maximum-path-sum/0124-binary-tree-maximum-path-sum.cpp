df69290e26e60d02a9aa3197656657140b6fa976
Rushikesh Ghotekar <rushikeshvghotekar25@gmail.com>
2022-12-11 13:06:47 +0530

    Time: 32 ms (82.34%), Space: 27.7 MB (35.19%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    int dfs(TreeNode* root, int& maxsum) {
+        if(!root) return 0;
+        int l = max(0,dfs(root->left,maxsum));
+        int r = max(0,dfs(root->right,maxsum));
+        maxsum = max(l+r+root->val, maxsum);
+        return root->val + max(l,r);
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxsum = INT_MIN;
+        dfs(root,maxsum);
+        return maxsum;
+    }
+};

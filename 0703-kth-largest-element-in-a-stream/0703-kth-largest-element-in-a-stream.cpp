fa7c59df4de5c8e30fbbdc118a8d40c13edb612e
Rushikesh Ghotekar <rushikeshvghotekar25@gmail.com>
2023-05-23 09:52:16 +0530

    Time: 33 ms (79.43%), Space: 19.7 MB (86.52%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,18 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>>pq;
+    int size;
+    KthLargest(int k, vector<int>& nums) {
+        size=k;
+        for(auto i:nums){
+         pq.push(i);
+         if(pq.size()>size)pq.pop();
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size()>size)pq.pop();
+        return pq.top();
+    }
+};
